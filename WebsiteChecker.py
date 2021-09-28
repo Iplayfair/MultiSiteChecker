f253a28d554b6e139df9694806e6f52cbd20b2df
Roman Brejninger <roman.brejniger@test.de>
2021-09-28 15:37:48 +0200

    Made a menue

--- WebsiteChecker.py
+++ WebsiteChecker.py
@@ -1,16 +1,14 @@
-from icmplib import ping,multiping,traceroute,resolve
+from icmplib import ping, multiping, traceroute, resolve
 
-auswahl = int(input("What you wanna do? \n Check connections (1) \n Delete connections (2) \n Add connections (3) "))
 
 def connections_check():
-    
+
     hosts = []
 
     with open("hosts.txt", "r") as file:
         for line in file:
             hosts.append(line.strip())
 
-    
     y = multiping(hosts)
 
     for host in y:
@@ -19,35 +17,36 @@ def connections_check():
         elif host.is_alive:
             print("Host is down")
 
+
 def connections_add():
 
     x = "Y"
 
-    with open ("hosts.txt", "a") as file:
+    with open("hosts.txt", "a") as file:
 
         while x == "Y":
 
             file.write(input("Which Website do you wanna check? " + "\n")+"\n")
             x = input("Do you wann check another website? (Y/N)")
+    menue()
 
 
 def connections_delete():
-    
+
     hosts = []
     x = "Y"
 
     with open("hosts.txt", "r") as file:
-        
+
         for line in file:
             hosts.append(line.strip())
 
-            
     while x == "Y":
 
-        print (hosts)
+        print(hosts)
 
         user_eingabe = input(str("Which Site you wanna delete? "))
-    
+
         if user_eingabe in hosts:
             removed_line = hosts.index(user_eingabe)
 
@@ -60,18 +59,30 @@ def connections_delete():
                 for line in lines:
                     f.write(line)
 
-        else: 
-            print ("The site does not exist")
-        
+        else:
+            print("The site does not exist")
+
         x = input(str("Do you wanna delete an another site? (Y/N)"))
 
+    menue()
 
 
-if auswahl == 1:
-    connections_check()
-    
-elif auswahl == 2:
-    connections_delete()
+def menue():
 
-else:
-    connections_add()
+    choose = int(input(
+        "What you wanna do? \n Check connections (1) \n Delete connections (2) \n Add connections (3) \n Quit (4)"))
+
+    if choose == 1:
+        connections_check()
+
+    elif choose == 2:
+        connections_delete()
+
+    elif choose == 3:
+        connections_add()
+
+    else:
+        quit()
+
+
+menue()
